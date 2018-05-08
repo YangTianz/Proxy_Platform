d6cce9359eebcea56387f8d78b202c467eff6a4a
Alpacaca <38241588+JohnyLi@users.noreply.github.com>
2018-05-08 15:25:36 +0800

    用于测试scheduler

--- src/test.py
+++ src/test.py
@@ -2,4 +2,5 @@ from Scheduler import *
 
 
 if __name__ == '__main__' :
-   Scheduler("http://www.bilibili.com/",time_max=3,time_delay=2,request_con=3,session=True)
+   Scheduler("http://www.bilibili.com/",time_max=3,time_delay=2,request_con=5,session=True) #访问的方法
+   #参数为： URL 网址， time_max 为 每个ip最大访问量 ， time_delay 为 每次访问间隔 ， request_con 为 任务并发数 ， session 为 Session功能
