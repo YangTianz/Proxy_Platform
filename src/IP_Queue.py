fb60b6b93ea2368f73397962e428630b1123c35f
Alpacaca <38241588+JohnyLi@users.noreply.github.com>
2018-05-08 15:11:08 +0800

    增加了一堆垃圾

---
+++ src/IP_Queue.py
@@ -0,0 +1,19 @@
+from queue import Queue
+import DBUtils
+
+class IP_Queue:#创建可用IP队列
+
+    def __init__(self):
+        self.__ip_queue=Queue()
+        ip_list=DBUtils.GetAvailableIP()
+        for ip in ip_list:
+            self.__ip_queue.put(ip)
+
+    def get_ip(self,block=True):
+        return self.__ip_queue.get(block=block)
+
+    def put_ip(self,ip):
+        self.__ip_queue.put(ip)
+
+    def empty(self):
+        return self.__ip_queue.empty()
---
+++ src/test.py
@@ -0,0 +1,5 @@
+from Scheduler import *
+
+
+if __name__ == '__main__' :
+   Scheduler("http://www.bilibili.com/",time_max=3,time_delay=2,request_con=3,session=True)
