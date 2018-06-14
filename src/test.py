ff846375c891301198af97d52f0c1805e2b53fe3
JohnyLi <491245006@qq.com>
2018-06-14 20:19:12 +0800

    2333

--- src/test.py
+++ src/test.py
@@ -4,11 +4,15 @@ from urllib import request
 from bs4 import BeautifulSoup
 if __name__ == '__main__' :
 
-
+            url="http://localhost/api/?url=https://www.etymonline.com/word/fuck"
+            response=request.urlopen(url)
+            text=response.read().decode()
+            print(text)
 
             url = "http://localhost/status"
             response = request.urlopen(url)
             text = response.read().decode()
+            text = json.loads(text)
             print(text)
 
 
