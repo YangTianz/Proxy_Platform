4c2ce884dfebd1c4981ba67326999bb79850f633
JohnyLi <491245006@qq.com>
2018-06-14 14:32:53 +0800

    233

--- src/test.py
+++ src/test.py
@@ -1,27 +1,15 @@
 # -*- coding: UTF-8 -*-
-
+import json
 from urllib import request
 from bs4 import BeautifulSoup
 if __name__ == '__main__' :
 
-            url="https://www.etymonline.com/word/fuck"
+            url="http://localhost/api/?url=https://www.etymonline.com/word/fuck&&time_max=2"
             response=request.urlopen(url)
-            text=response.read()
-            soup=BeautifulSoup(text,"html.parser")
-            sentence=""
-            laji=[]
-            for shit in soup.find_all('section', class_="word__defination--2q7ZH"):
-                laji.append(shit)
-            i=0
-            for fuck in soup.find_all(class_="word__name--TTbAA"):
-                sentence=sentence+fuck.get_text()+"\n"
-                now=laji[i]
-                for j in now.find_all('p'):
-                    sentence = sentence + j.get_text()
-                sentence=sentence+"\n"
+            text=response.read().decode()
+            text=json.loads(text)
+            print(text)
 
-                i=i+1
-            print(sentence)
 
 
 
