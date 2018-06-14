10871769dc57bb889d82a1c3daff5119c011b533
UwillNEVERKNW <32926228+UwillNEVERKNW@users.noreply.github.com>
2018-06-14 21:17:30 +0800

    over

--- src/Valid_check/Checker.py
+++ src/Valid_check/Checker.py
@@ -3,6 +3,7 @@ import time
 import gevent
 from gevent import monkey;monkey.patch_all()
 from Utils.redisdb import RedisClient
+from Valid_check.Headers import *
 
 requests.adapters.DEFAULT_RETRIES = 5       #设置最大重连次数
 socket.setdefaulttimeout(20)        #设置默认超时时间
@@ -14,21 +15,16 @@ verifyWeb = ["http://httpbin.org/ip",
              "https://www.douban.com",
              "https://stackoverflow.com"]
 
+sus = 0    # 记录每次操作可用
 
-headers = {
-    "Host": "www.douban.com",
-    "Connection": "keep-alive",
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.26 Safari/537.36 Core/1.63.5478.400 QQBrowser/10.1.1550.400",
-    "Upgrade-Insecure-Requests": "1",
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
-    "Accept-Encoding": "gzip, deflate, br",
-    "Accept-Language": "zh-CN,zh;q=0.9"
-
-}
-sus = 0
-
-# 可用性验证
-def validIP(IP,web,ips,test):
+"""
+可用性验证函数
+IP：str，需要验证的IP，格式为"IP:port"
+web：str，目标验证网站，格式为"http/https://xxx.xxxx.xxx"
+ips：IP实例，来自Utils.IP的class IP，进行增减分数操作对象
+IPct：RedisClient实例，来自Utils.redisdb的class RedisClient，包含增减分数操作函数
+"""
+def validIP(IP,web,ips,IPct):
 
     '''检查IP可用性'''
 
@@ -38,50 +34,60 @@ def validIP(IP,web,ips,test):
         'https': IP,
     }
 
-    '''一次基础校验'''
+    '''开始校验'''
     try:
-        #print("开始验证")
-        r = requests.get(web, proxies=proxies, timeout=(10, 10))      #设置超时
+
+        if web == "https://www.zhihu.com":
+            r = requests.get(web, proxies=proxies, headers=zhihu, timeout=(10, 5))      # 知乎需要header，否则一个也过不了
+        else:
+            r = requests.get(web, proxies=proxies, timeout=(10, 5))         # 一般情况
+
         if r.status_code == 200:
 
-            print("成功%s" %r.elapsed.seconds)
+            back = r.elapsed.seconds        # 响应时间
+            if back < 1:
+                IPct.increase(ips, 4)
+
+            elif 1 <= back < 3:
+                IPct.increase(ips, 3)
+
+            elif 3 <= back < 5:
+                IPct.increase(ips, 2)
+
+            elif 5 <= back < 7:
+                IPct.increase(ips, 1)
+
             global sus
             sus = sus + 1
 
         else:
-            test.decrease(ips)
-            #print("失败,异常结果为%s")
-
+            IPct.decrease(ips, 1)
 
     except:
-        test.decrease(ips)
-        #print("异常结果为%s" %res)
-
-
-
-'''检查端口是否关闭, 比较耗时'''
-def isOpen(ip, port):
-
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    try:
-        s.connect((ip, int(port)))
-        s.shutdown(2)
-        return True
-    except:
-        return False
+        IPct.decrease(ips, 1)
 
 
 if __name__ == "__main__":
 
-    test = RedisClient()
-    print(test.count())
-    ip = test.batch(0, 160)
+    IPct = RedisClient()
+    num = IPct.count()        # 库中IP数量
+    ip = IPct.batch(0, num)    # 获取数量
     js = []
-    for ips in ip:
 
-        IP = ips.getAddress() + ":" + str(ips.getPort())
-        t = gevent.spawn(validIP, IP, "https://www.douban.com", ips, test)
-        js.append(t)
+    """
+    持续进行验证，保证IP可用性
+    使用gevent，内置事件驱动的异步
+    """
+    while 1:
+        for web in verifyWeb:
+            for ips in ip:
+                IP = ips.getAddress() + ":" + str(ips.getPort())
+                t = gevent.spawn(validIP, IP, web, ips, IPct)
+                js.append(t)
 
-    gevent.joinall(js)
-    print(sus)
+            gevent.joinall(js)
+            print("当前验证网站可用数量为：%s" % sus)
+            sus = 0
+            time.sleep(10)
+
+        time.sleep(30)
---
+++ src/Valid_check/Headers.py
@@ -0,0 +1,17 @@
+
+"""
+headers支持特定网站验证
+"""
+
+zhihu = {
+    "authority": "www.zhihu.com",
+    "method": "GET",
+    "path": "/",
+    "scheme": "https",
+    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+    "accept-encoding": "gzip, deflate, br",
+    "accept-language": "zh-CN,zh;q=0.9",
+    "upgrade-insecure-requests": "1",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.26 Safari/537.36"
+}
+
